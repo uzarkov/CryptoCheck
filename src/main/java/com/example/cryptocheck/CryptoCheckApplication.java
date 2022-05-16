66f5ccf99c53f7a7eddaa52a53f220298ba6d234
Witold Brzozowski <brzozowskyy@gmail.com>
2022-05-17 01:32:06 +0200

    Add initial config

---
+++ src/main/java/com/example/cryptocheck/CryptoCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptocheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoCheckApplication.class, args);
+    }
+
+}
