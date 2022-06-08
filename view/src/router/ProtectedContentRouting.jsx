1b9c6007dd1c89ea5b44c41c5ec599ba943bd216
Witold Brzozowski <brzozowskyy@gmail.com>
2022-06-08 23:51:14 +0200

    Add first liquibase migration and some frontend tweaks

--- view/src/router/ProtectedContentRouting.jsx
+++ view/src/router/ProtectedContentRouting.jsx
@@ -1,4 +1,4 @@
-import {Redirect, Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch, useHistory} from "react-router-dom";
 import {AnalyticsPage, URL as AnalyticsURL} from "../pages/analytics-page/AnalyticsPage";
 import {DashboardPage, URL as DashboardURL} from "../pages/dashboard-page/DashboardPage";
 import {PortfolioPage, URL as PortfolioURL} from "../pages/portfolio-page/PortfolioPage";
@@ -6,12 +6,19 @@ import {PositionsPage, URL as PositionsURL} from "../pages/positions-page/Positi
 import {ProfilePage, URL as ProfileURL} from "../pages/profile-page/ProfilePage";
 import {ErrorPage} from "../pages/error-page/ErrorPage";
 import {Sidebar} from "../components/sidebar/Sidebar";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Topbar} from "../components/topbar/Topbar";
 import {Box, Container, Toolbar} from "@mui/material";
 
-export const ProtectedContentRouting = ({user, logout}) => {
+export const ProtectedContentRouting = ({user, logout, requestedLocation}) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const history = useHistory()
+
+  useEffect(() => {
+    if (requestedLocation) {
+      history.replace(requestedLocation)
+    }
+  }, [history, requestedLocation])
 
   return (
     <>
